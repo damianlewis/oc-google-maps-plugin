3e5882611d090af1cc6c8590681f4d5d385044df
Damian Lewis <damian@damianlewis.co.uk>
2019-11-18 15:16:28 +0000

    Add basic map

--- components/GoogleMap.php
+++ components/GoogleMap.php
@@ -2,14 +2,17 @@
 
 namespace DamianLewis\GoogleMaps\Components;
 
+use ApplicationException;
 use Cms\Classes\ComponentBase;
+use DamianLewis\GoogleMaps\Models\Settings;
 
 class GoogleMap extends ComponentBase
 {
+    public $apiKey;
     public function componentDetails()
     {
         return [
-            'name'        => 'Google Map Component',
+            'name' => 'Google Map',
             'description' => 'Display a Google Map on the page.'
         ];
     }
@@ -17,34 +20,64 @@ class GoogleMap extends ComponentBase
     public function defineProperties()
     {
         return [
-            'latitude'   => [
-                'title'   => 'Latitude',
-                'default' => 51.751623
+            'id' => [
+                'title' => 'ID',
+                'default' => 'googleMap',
+                'description' => 'The ID of the element the map should attached to'
             ],
-            'longitude'  => [
-                'title'   => 'Longitude',
-                'default' => -0.342130
+            'latitude' => [
+                'title' => 'Latitude',
+                'default' => -34.397,
+                'type' => 'string'
             ],
-            'zoom'       => [
-                'title'   => 'Zoom',
-                'default' => 12
+            'longitude' => [
+                'title' => 'Longitude',
+                'default' => 150.644,
+                'type' => 'string'
             ],
-            'mapTypeId'  => [
-                'title'   => 'Map Type',
+            'width' => [
+                'title' => 'Width',
+                'default' => '100%',
+                'type' => 'string'
+            ],
+            'height' => [
+                'title' => 'Height',
+                'default' => '350px',
+                'type' => 'string'
+            ],
+            'zoom' => [
+                'title' => 'Zoom',
+                'default' => 12,
+                'type' => 'string'
+            ],
+            'mapTypeId' => [
+                'title' => 'Map Type',
                 'default' => 'roadmap',
-                'type'    => 'dropdown',
+                'type' => 'dropdown',
                 'options' => [
-                    'roadmap'   => 'Roadmap',
+                    'roadmap' => 'Roadmap',
                     'satellite' => 'Satellite',
-                    'hybrid'    => 'Hybrid',
-                    'terrain'   => 'Terrain'
+                    'hybrid' => 'Hybrid',
+                    'terrain' => 'Terrain'
                 ]
             ],
             'showMarker' => [
-                'title'   => 'Show Marker',
+                'title' => 'Show Marker',
                 'default' => 'true',
-                'type'    => 'checkbox',
-            ]
+                'type' => 'checkbox',
+                'group' => 'Maker'
+            ],
         ];
+    }
+
+    public function onRun()
+    {
+        $settings = Settings::instance();
+
+        if (!$settings->api_key) {
+            throw new ApplicationException('Google Maps API key is not configured.');
+        }
+
+        $this->apiKey = $settings->api_key;
     }
 }
